c188fafa940b31475984798105fe8e2ffa93dc5e
murtaza98 <murtaza.patrawala@rocket.chat>
2022-02-20 16:49:20 +0530

    Initial Commit

---
+++ functions/license.js
@@ -0,0 +1,175 @@
+const MongoClient = require("mongodb").MongoClient;
+const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+
+// Database configurations
+const MONGODB_URI = process.env.MONGODB_URI;
+const DB_NAME = 'license';
+const LICENSE_COLLECTION = "license";
+
+// private key for the server
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const PRIVATE_KEY_PASSPHRASE = process.env.PRIVATE_KEY_PASSPHRASE;
+
+// email configuration
+const MAIL_SENDER = process.env.MAIL_SENDER;
+const MAIL_PASSWORD = process.env.MAIL_PASSWORD;
+
+let cachedDb = null;
+
+const connectToDatabase = async (uri) => {
+	// we can cache the access to our database to speed things up a bit
+	// (this is the only thing that is safe to cache here)
+	if (cachedDb) return cachedDb;
+
+	const client = await MongoClient.connect(uri, {
+		useUnifiedTopology: true,
+	});
+
+	cachedDb = client.db(DB_NAME);
+
+	return cachedDb;
+};
+
+const queryDatabase = async (db) => {
+	const pokemon = await db.collection("pokemon").find({}).toArray();
+
+	return {
+		statusCode: 200,
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(pokemon),
+	};
+};
+
+function addMinutes(date, minutes) {
+	return new Date(date.getTime() + minutes*60000);
+}
+
+const saveUserData = async (db, { email, workspaceAddress, license }) => {
+	console.log(`Saving license for email: ${email} and workspace address: ${workspaceAddress}`);
+	await db.collection(LICENSE_COLLECTION).insertMany([{
+		email,
+		workspaceAddress,
+		license,
+		createdAt: new Date(),
+		createdBy: 'netlify-functions',
+	}]);
+};
+
+const generateLicense = (data) => {
+	console.log(`Generating license for email: ${data.email} and workspace address: ${data.workspaceAddress}`);
+
+	const { workspaceAddress } = data;
+	// 30 days in future
+	const expiry = addMinutes(new Date(), 60*24*30);
+	const licenseData = {
+		url: workspaceAddress,
+		expiry,
+	};
+
+	const license = crypto.privateEncrypt({
+			key: Buffer.from(PRIVATE_KEY, 'base64'),
+			passphrase: PRIVATE_KEY_PASSPHRASE,
+		}, Buffer.from(JSON.stringify(licenseData), 'utf8'))
+		.toString('base64');
+
+	console.log('License generated: ' + license);
+
+	return { license, expiry };
+}
+
+const sendEmail = async (email, workspaceAddress, license, licenseExpiry) => {
+	console.log(`Sending email to ${email}`);
+	
+	const transporter = nodemailer.createTransport({
+		service: 'gmail',
+		auth: {
+			user: MAIL_SENDER,
+			pass: MAIL_PASSWORD,
+		}
+	});
+	
+	const mailOptions = {
+		from: MAIL_SENDER,
+		to: email,
+		subject: `Here's your Surveys App License`,
+		html: `<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
+		<div style="margin:50px auto;width:70%;padding:20px 0">
+		  <div style="border-bottom:1px solid #eee">
+			<a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Surveys App for Rocket.Chat</a>
+		  </div>
+		  <p style="font-size:1.1em">Hi,</p>
+		  <p>Thank you for choosing Surveys App. Here's your Premium License which you can setup following these steps. This license is valid until ${ licenseExpiry.toDateString() }.</p>
+		  <h3>Premium License</h3>
+		<table cellpadding="0" cellspacing="0" style="background: pink;">
+		  	<tr>
+				<td style="background: pink; color: #000; font-family: Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace; font-size: 13px; padding: 10px 15px;">
+					<pre style="-moz-tab-size: 2; -ms-hyphens: none; -o-tab-size: 2; -webkit-hyphens: none; color: #000; font-family: Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace; font-size: 13px; hyphens: none; line-height: 1.5; overflow: auto; tab-size: 2; text-align: left; white-space: pre; word-break: break-all; word-spacing: normal; word-wrap: normal;">${license}</pre>
+				</td>
+			</tr>
+		</table>
+		  <h3>License Setup Guide</h3>
+		  <ol type="1">
+			<li>Login to your Rocket.Chat server - <a href="${workspaceAddress}">${workspaceAddress}</a></li>
+			<li>Goto Apps Setting (<i>Administration</i> -> <i>Apps</i> -> Click on <i>Surveys App</i>)</li>
+			<li>Once you're on the app setting page, there locate the <i>Premium License</i> Setting and there add the above mentioned license key. Then just click on Save Settings and that's it. Your License has now been activated</li>
+			<li>(Optional) To verify if the license is set, just try out any Premium features like sending recurring poll</li>
+		  </ol>
+			<h4> Facing any problems?</h4>
+			Feel free to contact us anytime at this email - surveysinchat@gmail.com - and we'd be happy to help you
+		<p style="font-size:0.9em;">Regards,<br />Surveys App Team</p>
+		  <hr style="border:none;border-top:1px solid #eee" />
+		</div>
+	  </div>`
+	};
+	
+	const result = await transporter.sendMail(mailOptions);
+	console.log(`Email sent: ${JSON.stringify(result)}`);
+}
+	
+module.exports.handler = async (event, context) => {
+		// otherwise the connection will never complete, since
+		// we keep the DB connection alive
+		context.callbackWaitsForEmptyEventLoop = false;
+
+		const db = await connectToDatabase(MONGODB_URI);
+
+		// enable CORS
+		const corsHeaders = {
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Headers': 'Content-Type',
+			'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+		}
+		if (event.httpMethod === "OPTIONS") {
+			return {
+				statusCode: 200,
+				headers: corsHeaders,
+				body: 'Cross site requests allowed!'
+			}
+		}
+
+		if (event.httpMethod !== "POST") {
+			return { statusCode: 400, body: "Method not allowed" };
+		}
+
+		const data = JSON.parse(event.body);
+		if (!data.email || !data.workspaceAddress) {
+			return { statusCode: 422, body: "Missing email or workspace address" };
+		}
+
+		const { license, expiry } = generateLicense(data);
+		data.license = license;
+
+		await sendEmail(data.email, data.workspaceAddress, license, expiry);
+
+		await saveUserData(db, data);
+
+		console.log(`Successfully generated license & email it to: ${JSON.stringify(data)}`);
+
+		return {
+			statusCode: 200,
+			headers: corsHeaders,
+		};
+};
