09b7fc8857a488813dec27c5b4556daaf5388ccf
murtaza1998 <murtaza98.test@gmail.com>
2022-12-04 17:18:15 +0530

    add debug logs

--- netlify/functions/test_email.ts
+++ netlify/functions/test_email.ts
@@ -5,11 +5,15 @@ import { getZohoContactMailer } from "./lib/email/emailProvider";
 // time to get initialized, I'm going to try to initialize it here
 // Possibly need to call this routinely to keep it alive?
 const handler: Handler = async (event, context) => {
+  console.debug(`Starting to send email at ${new Date()}`);
+
   await getZohoContactMailer().sendEmail(
     "murtaza98.test@gmail.com",
     "Test Email",
     "This is a test email"
   );
+
+  console.debug(`Finished sending email at ${new Date()}`);
 
   return {
     statusCode: 200,
