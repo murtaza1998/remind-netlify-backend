28b7d78eefaf7f8f2829cce630e4d522452a7968
Murtaza Patrawala <34130764+murtaza98@users.noreply.github.com>
2023-01-10 11:38:54 +0530

    [IMPROVE] Env variables & cleanup (#6)
    Co-authored-by: murtaza1998 <murtaza98.test@gmail.com>

--- netlify/functions/lib/configs/envVariables.ts
+++ netlify/functions/lib/configs/envVariables.ts
@@ -1,11 +1,7 @@
 // Due to 4kb limit on Netlify functions, we need to use a separate file for env variables :(
 export const ENV_VARIABLES = {
-  PADDLE_PLAN_MONTHLY_ID: "40758",
-  PADDLE_PLAN_YEARLY_ID: "40418",
   ZOHO_SMTP_HOST: "smtp.zoho.in",
   ZOHO_SMTP_PORT: "465",
-  MAIL_SENDER: "remindersinchat@gmail.com",
-  CONTACT_US_EMAIL_NOTIFICATION: "remindersinchat@gmail.com",
   CONTACT_APPSFORCHAT_FROM_MAIL: "contact@appsforchat.com",
   CONTACT_APPSFORCHAT_MEMBER_MAIL: "murtaza.patrawala@appsforchat.com",
   TRIAL_MONTH: "1",
--- netlify/functions/lib/paddle/plan.ts
+++ netlify/functions/lib/paddle/plan.ts
@@ -1,10 +1,13 @@
-import { ENV_VARIABLES } from "../configs/envVariables";
+import { env } from "process";
 
 class PaddlePlanClass {
   getPlanName(planId: string): "Monthly" | "Yearly" {
-    const { PADDLE_PLAN_MONTHLY_ID, PADDLE_PLAN_YEARLY_ID } = ENV_VARIABLES;
-    if (!PADDLE_PLAN_MONTHLY_ID || !PADDLE_PLAN_YEARLY_ID) {
-      throw new Error("Paddle plan IDs not set");
+    const { PADDLE_PLAN_MONTHLY_ID, PADDLE_PLAN_YEARLY_ID } = env;
+    if (!PADDLE_PLAN_MONTHLY_ID) {
+      throw new Error("Paddle monthly plan ID not set");
+    }
+    if (!PADDLE_PLAN_YEARLY_ID) {
+      throw new Error("Paddle yearly plan ID not set");
     }
 
     if (planId === PADDLE_PLAN_MONTHLY_ID) {
