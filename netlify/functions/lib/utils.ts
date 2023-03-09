34fa2e48fbdf88ddad35f65c3fa0e77fe0cb52a7
Murtaza Patrawala <34130764+murtaza98@users.noreply.github.com>
2023-03-09 19:13:44 +0530

    Chore: add buffer for 7 days for license expiration (#8)
    Co-authored-by: murtaza1998 <murtaza98.test@gmail.com>

--- netlify/functions/lib/utils.ts
+++ netlify/functions/lib/utils.ts
@@ -5,3 +5,9 @@ export const removeTrailingSlashFromUrl = (url: string): string => {
 export const sleep = (ms: number): Promise<void> => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
+
+export const addDaysToDate = (date: Date, days: number): Date => {
+  const newDate = new Date(date);
+  newDate.setDate(newDate.getDate() + days);
+  return newDate;
+};
